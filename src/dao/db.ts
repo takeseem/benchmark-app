f810b441153e27387bb3b0e78a0a4b1394c227f6
takeseem <takeseem@gmail.com>
2025-02-18 06:55:24 +0800

    for debug

--- src/dao/db.ts
+++ src/dao/db.ts
@@ -5,11 +5,13 @@ const connectionString = process.env.DATABASE_URL!;
 console.log(`Connecting to ${connectionString}`);
 
 const url = URL.parse(process.env.DATABASE_URL!)!;
+const ssl = {
+  ca: url.searchParams.get('sslrootcert')!,
+  key: url.searchParams.get('sslkey')!,
+  cert: url.searchParams.get('sslcert')!,
+};
+console.log(`SSL: ${JSON.stringify(ssl)}`);
 export const pool = new pg.Pool({
   connectionString,
-  ssl: {
-    ca: url.searchParams.get('sslrootcert')!,
-    key: url.searchParams.get('sslkey')!,
-    cert: url.searchParams.get('sslcert')!,
-  }
+  ssl: ssl,
 });
